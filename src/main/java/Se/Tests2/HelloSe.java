1106439b5abe79238df8f52a6bed1956b8c7f567
srmohanreddy <srmohanreddy@gmail.com>
2018-06-13 15:49:47 +0530

    DriverManager updated

--- src/main/java/Se/Tests2/HelloSe.java
+++ src/main/java/Se/Tests2/HelloSe.java
@@ -4,18 +4,24 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class HelloSe {
 
 	    private WebDriver driver;
 //Comment added
 	    @BeforeClass
 	    public void beforeClass() {
-	        driver = new FirefoxDriver();
+	      //  driver = new FirefoxDriver();
+	        WebDriverManager.chromedriver().setup();
+        	driver = new ChromeDriver();
+	        
 	    }
 
 	    @AfterClass
